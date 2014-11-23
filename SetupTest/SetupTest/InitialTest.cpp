102ffe9bcd0346c755c9843d4876de6f71744f6b
Michael Zuccarino <mikezuccarino@gmail.com>
2014-11-23 01:40:49 -0800

    some porting work

--- SetupTest/SetupTest/InitialTest.cpp
+++ SetupTest/SetupTest/InitialTest.cpp
@@ -1,5 +1,6 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/calib3d/calib3d.hpp>calib3d.hpp
 #include <iostream>
 
 using namespace cv;
@@ -8,19 +9,29 @@ using namespace std;
 int main()
 {
 	namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
-
-	Mat image;
-	image = imread("C:\\Users\\mikez_000\\Documents\\GitHub\\opencv-tests\\SetupTest\\SetupTest\\diagonalcenter.jpg", IMREAD_COLOR); // Read the file
-
-	if (!image.data) // Check for invalid input
-	{
-		cout << "Could not open or find the image" << std::endl;
-		waitKey(0); // Wait for a keystroke in the window
-		return -1;
+	CvCapture* capture = cvCaptureFromCAM(0);
+	cout << "open capture" << endl;
+	IplImage *img;
+	Size boardSize(5, 4);
+	vector<Point2f> corners;
+	
+	while (1) {
+		cout << "start while" << endl;
+		img = cvQueryFrame(capture);
+		cv::Mat imgmat(img, false);
+		if (findChessboardCorners(imgmat, Size(5, 4), corners, CALIB_CB_ADAPTIVE_THRESH + CALIB_CB_NORMALIZE_IMAGE))
+		{
+			drawChessboardCorners(imgmat, boardSize, Mat(corners), true);
+			calibrateCamera()
+		}
+		cout << "query frame" << endl;
+		if (!img) break;
+		//imshow("Example2", frame);
+		imshow("Example2", imgmat);
+		char c = cvWaitKey(33);
+		if (c == 27) break;
 	}
-
-	imshow("Display window", image); // Show our image inside it.
-
-	waitKey(0); // Wait for a keystroke in the window
+	cvReleaseCapture(&capture);
+	cvWaitKey(0);
 	return 0;
 }
